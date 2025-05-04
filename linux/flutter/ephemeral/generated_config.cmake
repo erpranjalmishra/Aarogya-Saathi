d847e10d435da7ccad415d2ba5a9d1eb28fe93b9
Pranjal Mishra <x@pranjalmishra.dev>
2025-05-04 22:59:15 +0530

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/cherry-lily/development/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/cherry-lily/Documents/coding/flutter/aarogya_saathi" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/cherry-lily/development/flutter"
+  "PROJECT_DIR=/home/cherry-lily/Documents/coding/flutter/aarogya_saathi"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/cherry-lily/Documents/coding/flutter/aarogya_saathi/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/cherry-lily/Documents/coding/flutter/aarogya_saathi/lib/main.dart"
+)
